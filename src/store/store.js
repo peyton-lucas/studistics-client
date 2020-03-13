2fc8d9b6f3d55c9ff023b460740fd4b2021858b0
peyton-lucas <camplucastein@gmail.com>
2020-03-13 16:59:04 -0500

    Removing redux devtools extension

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import { combineReducers, createStore, compose, applyMiddleware } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { authenticateUser, fetchUserData } from "./reducers";
 import thunk from "redux-thunk";
 
@@ -10,8 +9,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+  compose(
+    applyMiddleware(thunk)));
 
 export default store;
