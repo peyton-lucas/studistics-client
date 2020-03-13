bc4c38c717fc5e6af6bcebfff0eb1ac3b4b8cc24
peyton-lucas <camplucastein@gmail.com>
2020-03-13 16:30:15 -0500

    Adding composeWithDevTools module to redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { combineReducers, createStore, compose, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { authenticateUser, fetchUserData } from "./reducers";
 import thunk from "redux-thunk";
 
@@ -9,7 +10,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  compose(
+  composeWithDevTools(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
